c499b7ee55f9349f0ef87abc21dd0e9898617170
Larry Bucio <l.bucio@aggiemail.usu.edu>
2018-10-09 07:56:41 -0600

    feat: Cache on fetch and save to cache from network

--- sw.js
+++ sw.js
@@ -15,3 +15,36 @@ self.addEventListener('install', (event) => {
         return cache.addAll(URLS_TO_CACHE);
     }());
 });
+
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('mws_restaurant_') &&
+                        cacheName != STATIC_CACHE_NAME;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        })
+    );
+
+    const request = event.request;
+    event.waitUntil(
+        caches.open(STATIC_CACHE_NAME).then(function (cache) {
+            return fetch(request).then(function (response) {
+                return cache.put(request, response);
+            });
+        })
+    );
+});
