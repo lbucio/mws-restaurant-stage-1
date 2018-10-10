2f9ce8a55e44f2ad11aa3eece152ddeb74993132
Larry Bucio <l.bucio@aggiemail.usu.edu>
2018-10-09 18:33:51 -0600

    fix: Change headings to use the correct hierarchy

--- js/main.js
+++ js/main.js
@@ -190,7 +190,7 @@ createRestaurantHTML = (restaurant) => {
   const content = document.createElement('div');
   content.className = 'card-content';
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   content.append(name);
 
